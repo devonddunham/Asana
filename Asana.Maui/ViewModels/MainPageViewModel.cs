9a114c6918da66decd18d612954ecdcce6b46185
Devon Dunham <itisdevondunham@gmail.com>
2025-06-09 16:02:07 -0400

    error checking assembly reference

---
+++ Asana.Maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+using Asana.Library.Models;
+using Asana.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Maui.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private ToDoServiceProxy _toDoSericeProxy;
+
+        public MainPageViewModel()
+        {
+            _toDoSericeProxy = ToDoServiceProxy.Current;
+        }
+
+        public ObservableCollection<ToDo> ToDos
+        {
+            get
+            {
+                var toDos = _toDoSericeProxy.ToDos;
+                if(!IsShowCompleted)
+                {
+                    toDos = _toDoSericeProxy.ToDos.Where(t => !t?.IsCompleted ?? false).ToList();
+                }
+                return new ObservableCollection<ToDo>(toDos);
+            }
+        }
+
+        private bool isShowCompleted;
+        public bool IsShowCompleted { 
+            get
+            {
+                return isShowCompleted;
+            }
+
+            set
+            {
+                if (isShowCompleted != value)
+                {
+                    isShowCompleted = value;
+                    NotifyPropertyChanged(nameof(ToDos));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
